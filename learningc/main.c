c23ffaf3053b5828b8ee71cbea2d5635ca0e5e1d
petrishinvlad <petrishin.vladislav@gmail.com>
2025-07-30 11:36:32 +0100

    Initial Commit

---
+++ learningc/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  learningc
+//
+//  Created by Vlad Petrishin on 30.07.2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
